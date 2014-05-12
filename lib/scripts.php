ed4a046d257f0046a80bf9baf04739137b4f6cd1
tgelles <tgelles@pha.jhu.edu>
2014-05-12 13:05:57 -0400

    Adding updated less files to change font size from px to em

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e1bd7bacc3d045dd79812be016e10fed');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e5d679c23adddbe5c22f1cd7d216db18');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
