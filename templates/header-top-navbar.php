a874c557ab2a084f576124ddc9211ca7ea2c84d8
tgelles <tgelles@pha.jhu.edu>
2014-05-13 09:36:41 -0400

    added better logo to navbar. resized navbar items so logo doesnt break line

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -5,7 +5,16 @@
              <!-- Button for smallest screens -->
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> 
                 </button>
-                <a class="navbar-brand" href="<?php echo home_url(); ?>/"><img src="/wp-content/uploads/2014/05/logo-sdss-1b.png" alt="sdsslogo" style="width:200px"></a>
+                <a class="navbar-brand" href="<?php echo home_url(); ?>/">
+<?php if (is_front_page() && !is_paged()) : ?>
+
+  <img src="/wp-content/uploads/2014/05/sdsslogowhite.png" alt="sdsslogo" style="width:200px">
+<?php else: ?>
+  <img src="/wp-content/uploads/2014/05/sdsslogo.png" alt="sdsslogo" style="width:200px">
+
+<?php endif; ?>
+ 
+                 </a>
             </div>
               <div class="navbar-collapse collapse"  role="navigation">
                 <?php
