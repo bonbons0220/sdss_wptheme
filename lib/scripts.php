604db322a48f0976d7dfaba1ac225e2716b26e13
tgelles <tgelles@pha.jhu.edu>
2014-05-20 14:36:35 -0600

    Added TODO contextual background class to type.less & variables.less

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'fbd32e36911cb4485397bf5b5a2efa86');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b925dcf9dd26a6a2b9be8e8b147819e0');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
