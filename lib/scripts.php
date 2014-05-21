4a1f19924360cc13b1e787a7269002f65e1db8f2
tgelles <tgelles@pha.jhu.edu>
2014-05-21 10:08:00 -0600

    Changed navbar color from .4 to .7 transparency

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b925dcf9dd26a6a2b9be8e8b147819e0');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5dc4e9c16b26c058a73a0a2814e5ce56');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
