18682192a509c685eba7c21296de7ede12a01091
tgelles <tgelles@pha.jhu.edu>
2014-05-27 13:23:20 -0400

    Condensed Sitemap to only display live pages on SDSS or SDSS-III site

--- front-page.php
+++ front-page.php
@@ -1,3 +1,2 @@
 <?php get_template_part('templates/content', 'page'); ?>
 <?php get_template_part('templates/page', 'footer'); ?>
-<!-hello john! -->
--- templates/sitemap.php
+++ templates/sitemap.php
@@ -5,11 +5,10 @@
 				<div class="col-xs-6 col-sm-6 col-md-3 col-lg-4 column">
 			  		<h5>Explore</h5>
 						<ul class="nav nav-pills nav-stacked">
-							<li><a href="/data-access/dr11-scope">Latest Data Release</a>  </li> 
-							<li><a href="/data-access/imaging-data">Imaging Data</a></li>
-							<li><a href="/data-access/sdss-spectroscopic-data">Optical Data</a></li>
-							<li><a href="/data-access/infrared-spectroscopic-data">Infrared Spectroscopic Data</a></li>
-							<li><a href="/image-gallery">Image Gallery</a></li>
+							<li><a href="/data-access/">Latest Data Release</a>  </li> 
+							<li><a href="http://www.sdss3.org/dr10/imaging/">SDSS-III Imaging Data</a></li>
+							<li><a href="http://www.sdss3.org/dr10/spectro/">SDSS-III Optical Data</a></li>
+							<li><a href="http://www.sdss3.org/dr10/irspec/">SDSS-III Infrared Spectroscopic Data</a></li>
 						</ul>
 				</div>
 				<div class="col-xs-6 col-sm-6 col-md-3 col-lg-4 column">
@@ -17,15 +16,13 @@
 						<ul class="nav nav-pills nav-stacked">
 							<li><a href="/surveys">Surveys</a></li>
 							<li><a href="/instruments">Instruments</a></li>
-							<li><a href="/data-access/help">Help & Tutorials</a></li>
-							<li><a href="/data-access/software">Software</a></li>
 							<li><a href="/education">Education</a></li>
+							<li><a href="/results-science">Results & Science</a></li>
 						  </ul>
 				</div>
 				<div class="col-xs-6 col-sm-6 col-md-3 col-lg-4 column">
 					<h5>About</h5>
 						<ul class="nav nav-pills nav-stacked">
-							<li><a href="/results-science">Results & Science</a></li>
 							<li><a href="/collaboration">Collaboration</a></li>
 							<li><a href="/collaboration/image-use-policy">Image Use Policy</a></li>
 							<li><a href="/collaboration/publication-policy">Publication Policy</a></li>
@@ -35,10 +32,12 @@
 			</div>
 			<div class="col-md-4">
 				<h5>SDSS is supported by</h5>
+				<div class="row">
 					<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 column"><a href="http://www.sloan.org/"><img src="http://cinema.usc.edu/archivedassets/038/9424.jpg" alt="sloan logo" class="img-responsive"></a></div>
 					<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 column"><a href="http://www.sdss3.org/images/home/logo_nsf.jpg"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/0/03/NSF_Logo.jpg/600px-NSF_Logo.jpg" alt="nsf logo" class="img-responsive"></a></div> 
-					<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 column"><a href="http://www.sdss3.org/images/home/logo_energy.jpg"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/US-DeptOfEnergy-Seal.svg/900px-US-DeptOfEnergy-Seal.svg.png" alt="doe-logo" class="img-responsive"></a>
-					</div>
+					<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 column"><a href="http://www.sdss3.org/images/home/logo_energy.jpg"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/US-DeptOfEnergy-Seal.svg/900px-US-DeptOfEnergy-Seal.svg.png" alt="doe-logo" class="img-responsive"></a></div>
+				</div>
+					<small>Funding for SDSS has been provided by the Alfred P. Sloan Foundation, the Participating Institutions, the National Science Foundation, and the U.S. Department of Energy Office of Science.</small>
 			</div>
 			</div>
 			</div>
