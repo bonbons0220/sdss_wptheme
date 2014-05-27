9434b1ad26012f6b614ade1934ddd5c3934cd215
tgelles <tgelles@pha.jhu.edu>
2014-05-27 16:53:41 -0400

    Fixed navbar brand and collapse line breaking issue. Navbar now fully collapses into .9 transparent background at XS width. Logo now responsive; no longer using fixed-width

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f56d4f60016fcff03d6af642253ce7dc');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c0b4ff85abc93931f4fa11b15f61d223');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
