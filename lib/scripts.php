8c5d7d0b197937ecffbb85b3a636547ea9ea9536
tgelles <tgelles@pha.jhu.edu>
2014-05-27 13:51:41 -0400

    Removed uppercase stylings from HP circle headings

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c46d955e1526986e6bc6a61ff5d6631b');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f56d4f60016fcff03d6af642253ce7dc');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
