c4b4826a46c0bf9773435d851f59a517e4b9ca72
tgelles <tgelles@pha.jhu.edu>
2014-05-29 16:47:17 -0400

    Resized and optimized telescope image for HP Surveys section

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '35474af76f569fd53c3d1e7b446987c5');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7957da7b2684a8eddbe653321a8fabf8');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
