5695851af50662c76b11286adae68418c5034354
tgelles <tgelles@pha.jhu.edu>
2014-05-30 16:49:10 -0400

    added .nav.nav-list>.active>a {   border-left: 5px solid #6600ff; }

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9b95465aa269ff3835eb1f029dd1257d');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9b8c94a4b7c9563e95e7740dc866fb5a');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
