69a7ca1ebd6b8a6885004156828d47e435a1301f
tgelles <tgelles@pha.jhu.edu>
2014-06-09 16:13:59 -0400

    added 'a.back-to-top' class

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f9de8f8319b8b46314e611bf67a891ed');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ce3c87fd3ec3cfb40cedeb3f5ec74328');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
