c80d0950022fbcd6842050a6141bf0ebac99ec4c
tgelles <tgelles@pha.jhu.edu>
2014-06-09 14:21:47 -0400

    changed HP survey icons

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '485d6e6f0d2ae26671488ed1f31bae7d');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1c9372d99a09db7ef8bea7ae9994c40f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
