cb44a4b5d7453793b1dcf6e0b43d5d9d8efdf89b
tgelles <tgelles@pha.jhu.edu>
2014-06-10 15:52:26 -0400

    added dr12 custom post type php if statement and classes to content.php file

--- templates/content.php
+++ templates/content.php
@@ -1,3 +1,20 @@
+  <?php if (is_tax()): ?> 
+
+
+<article <?php post_class(); ?>>
+  <header>
+    <h2 class="entry-title"><?php the_title(); ?></h2>
+    <!--Remove meta data
+    <?php get_template_part('templates/entry-meta'); ?>-->
+  </header>
+  <div class="entry-summary">
+    <?php the_content(); ?>
+  </div>
+</article>
+
+
+ <?php else: ?>
+
 <article <?php post_class(); ?>>
   <header>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
@@ -7,3 +24,5 @@
     <?php the_excerpt(); ?>
   </div>
 </article>
+
+ <?php endif; ?>
