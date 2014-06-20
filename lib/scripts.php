4f33a60890b2ab53e953db53ebee6f0eb0e5e354
tgelles <tgelles@pha.jhu.edu>
2014-06-20 16:37:31 -0400

    reduced size of p.lead class

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '525f5698ae7911058b72c02a0ffec27d');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '472d4fa653995fd486b9a54e71fd4d9c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
