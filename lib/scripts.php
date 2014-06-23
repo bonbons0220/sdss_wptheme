41e4ebcede125384ff83cb4430bdddc18e064f62
tgelles <tgelles@pha.jhu.edu>
2014-06-23 09:52:23 -0400

    added max-width selector to .navbar-brand img class specific to media queries

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '472d4fa653995fd486b9a54e71fd4d9c');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '20d601eac93799f4e5691fdb0d88c64a');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
