94ddc9119bf1a9b5b67185fb6396f590b85054bb
tgelles <tgelles@pha.jhu.edu>
2014-06-25 16:16:52 -0400

    changed navbar item, active, and hover classes.

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '89cf035982ea43391a7269333faabf51');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8c716d251a91ef377785831d5ce8bf0a');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
