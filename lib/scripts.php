3f13b493881dd7c268e56f5188bfdc3a9ded1d66
tgelles <tgelles@pha.jhu.edu>
2014-06-26 09:00:24 -0400

    changed navbar inverse stylings. removed most redunant styles from app.less file; updated variables and navbar.less files to reflect sdss brand with active and hover states

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8c716d251a91ef377785831d5ce8bf0a');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '716786607fe614b79f27809f69e73d32');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
