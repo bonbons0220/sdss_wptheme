8d385060d699f8111aa2c8d658f004e046a05f89
tgelles <tgelles@pha.jhu.edu>
2014-07-15 15:53:40 -0400

    temporarily reverted content.php file to original

--- templates/content.php
+++ templates/content.php
@@ -1,15 +1,14 @@
 <?php if (is_singular( array ('algorithms', 'opticalspectra', 'data', 'imaging', 'infrared', 'software', 'help', 'tutorials', 'marvels', ) ) || is_post_type_archive( array ('algorithms', 'opticalspectra', 'data', 'imaging', 'infrared', 'software', 'help', 'tutorials', 'marvels' ) )): ?>
-<div class="post-block">
-  <article <?php post_class('press'); ?>>
-        <div class="content">
-          <h4 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-          <div class="entry-summary">
-              <?php the_excerpt(); ?>
-              <a href="<?php the_permalink(); ?>" class="btn btn-sm btn-sdss">Explore</a>
-          </div>
-        </div>
-  </article>
-</div>
+
+<article <?php post_class(); ?>>
+  <section class="sdss-docs-section">
+    <h3 class="entry-title" id="<?php $text=get_the_title(); $text=explode(' ',$text); echo strtolower($text[0]); ?>"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+  <div class="entry-summary">
+    <?php the_excerpt(); ?>
+  </div>
+  </section>
+</article>
+
 
  <?php else: ?>
 <!--Following renders the /Press page as buckets-->
