205b7423be6e106e49ffb323fc78851db5a8f39a
tgelles <tgelles@pha.jhu.edu>
2014-07-15 15:32:19 -0400

    reduced heading size of archive content from h3 to h4

--- templates/content.php
+++ templates/content.php
@@ -2,7 +2,7 @@
 <div class="col-sm-6">
   <article <?php post_class('press'); ?>>
         <div class="content">
-          <h3 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+          <h4 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
           <div class="entry-summary">
               <?php the_excerpt(); ?>
               <a href="<?php the_permalink(); ?>" class="btn btn-sm btn-sdss">Explore</a>
