abe3a0531757a8b3d1bfe046f0e36dd48e721961
bonbons0220 <bsouter@jhu.edu>
2014-12-10 12:04:50 -0500

    updated styles for menus. added top specta bar for secondary spectra menu items

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ee81016bbdb60ff04d16eedf644a8e52');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8f2758ce2f59b4d8d7297bddb36c5dad');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
