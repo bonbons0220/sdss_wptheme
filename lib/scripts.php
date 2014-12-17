ccbf8bac718f734a6bc5e1de7d681c5c3d2adabb
bonbons0220 <bsouter@jhu.edu>
2014-12-17 16:06:38 -0500

    Align figures center in sdss-figure. Adjust size of responsive video size.

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b9c8d5a1647929a61fc738cb34472a01');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b5fae11d6e5a77ccd9650115e6ee8e62');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
