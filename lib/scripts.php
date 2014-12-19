58387136cd75d2cbe93f4a4c18366768ca6fdd5f
bonbons0220 <bsouter@jhu.edu>
2014-12-19 14:37:14 -0500

    Added dl-horizontal-reverse and fixed so that multiple lines on the left don't cause vertical misalignment.

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd00c2f5600e5906a5a9197eb35d82f24');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '22e3011dee40464c208c6cf4e48fd6d4');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
