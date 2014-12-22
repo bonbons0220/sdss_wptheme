5cb09f44f5c68adc315cc9a571873cb10cdacd01
bonbons0220 <bsouter@jhu.edu>
2014-12-22 11:57:01 -0500

    fixed bug in removing padding from full width columns in sdss-groups

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c3eb3d51ff6aba9de348c9a72df2ff67');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'bd5fa5823fee09edb570fbaeeecbeaf0');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
