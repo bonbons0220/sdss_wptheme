c753bfd59f8b79e8fcf50eb0fbeb2ebdddc80fe1
bonbons0220 <bsouter@jhu.edu>
2014-12-22 13:21:47 -0500

    added right align for btn-sdss-right to save space and offset buttons

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c0a00dd6945118888393f38cdcbc90db');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '41a7f2b00d335492b646606852a51c05');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
