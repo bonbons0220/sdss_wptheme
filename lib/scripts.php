330756460718e49aa34bb8d990a299a890e2ae4b
bonbons0220 <bsouter@jhu.edu>
2014-12-22 14:01:49 -0500

    fixed dl-horizontal for groups as well as stories

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '41a7f2b00d335492b646606852a51c05');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ec1c5a62b3151fabf43fc507abe5fdc6');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
