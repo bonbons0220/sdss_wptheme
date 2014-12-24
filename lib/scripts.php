2799392afec347ed86468f95d1e47a026772eb3e
bonbons0220 <bsouter@jhu.edu>
2014-12-23 19:52:32 -0500

    fixed bottom margin above footer. added style for acknowledgement font

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ec1c5a62b3151fabf43fc507abe5fdc6');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4ad1c5debc0a7919a441ed278d3ba8c8');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
