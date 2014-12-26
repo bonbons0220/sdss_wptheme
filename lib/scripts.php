faca64cc2587266e81a0ab3e606b8f77b759e3f7
bonbons0220 <bsouter@jhu.edu>
2014-12-26 11:24:51 -0500

    made table captions more like h4

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b2d3e42728f963c3fbfb1bd43f606cc9');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ee208f50e2ac69145f390402400ef994');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
