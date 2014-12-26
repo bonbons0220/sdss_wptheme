8f5d8ab0172399306d09f730595ef6df590b726f
bonbons0220 <bsouter@jhu.edu>
2014-12-26 13:02:20 -0500

    STYLE UL IN sdss-groups as well as stories

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '825725298c65fcb6ac312592d2eeacfe');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'de033bd87accc23bb7faacdd14a86f53');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
