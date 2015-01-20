36f25fbd4ada4914f38b5f1fc5f804e7cc2294d2
bonbons0220 <bsouter@jhu.edu>
2015-01-20 14:04:25 -0500

    Added shortcode for todo. Added google tag manager code to base.php and base-template-custom.php

--- base-template-custom.php
+++ base-template-custom.php
@@ -1,5 +1,19 @@
 <?php get_template_part('templates/head'); ?>
 <body <?php body_class(); ?>>
+<?php 
+// Bonnie Souter 01/20/2015
+// Adding Google Tag Manager to manage Google Analytics and more without 
+// constantly updating analytics scripts and includes. 
+?>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-M6F94N"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M6F94N');</script>
+<!-- End Google Tag Manager -->
 
   <!--[if lt IE 8]>
     <div class="alert alert-warning">
