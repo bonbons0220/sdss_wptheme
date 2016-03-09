298dce5a1dd65ae34bcc4ba12b4d7b4aec1a736b
bonbons0220 <bsouter@jhu.edu>
2016-03-09 11:37:25 -0500

    added external class, fordr12, fordr13 classes

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ae78e19f22f6b7c1d974e884ea824c71');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '619c5740c68965c2d37aa19b415407ee');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
