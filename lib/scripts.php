bc553d937ada593db35e71463b5c51970d1e0f9c
bonbons0220 <bsouter@jhu.edu>
2016-05-13 17:44:35 -0400

    changed main logo link to website root instead of bloginfo root'

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '34545784bb37d595abe7623902781406');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '19ba6f88d5f212a22375f324a567bc3e');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
