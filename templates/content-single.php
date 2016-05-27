0866de2b5883648d35c2f654b636e0e565b215ea
bonbons0220 <bsouter@jhu.edu>
2016-05-27 15:30:16 -0400

    updated single post style for press releases

--- templates/content-single.php
+++ templates/content-single.php
@@ -3,7 +3,7 @@
 <article <?php post_class(); ?>>
 <header>
       <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php //get_template_part('templates/entry-meta'); ?>
+      <?php get_template_part('templates/entry-meta'); ?>
 </header>
 <div class="entry-content">
 <div class="row"><div class="col-xs-12">
